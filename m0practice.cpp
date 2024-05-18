b0ae6725fffaa0b6db4994413dc90f2c0fe480da
Pulkit Shringi <pulkitshringi@yahoo.com>
2024-05-18 14:51:07 +0530

    up

---
+++ m0practice.cpp
@@ -0,0 +1,7 @@
+#include <iostream> 
+using namespace std;
+int main(){
+if(cout<<"Hi "){
+    cout << "Pulkit" << endl;
+}
+}
--- m9while.cpp
+++ m9while.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
 using namespace std;
 int main (){
-    int i = 0;
-    while(i<10){
-        cout << i << endl;
-        i++;
-    }
+   char c = 'a';
+   for(;c<='z';c++){
+    cout << c << " ";
+   }
     return 0;  
 }
