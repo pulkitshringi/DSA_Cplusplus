e08473d314674ae6a2a83afc2fba96a15e1a8bb9
Pulkit Shringi <pulkitshringi@yahoo.com>
2024-05-28 23:49:14 +0530

    updated

---
+++ practice.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    for(int i=0;i<3;i++){
+        cout << "--> ";
+        for(int j=0;j<2;j++){
+            cout << "*";
+        }
+        cout << endl;
+    }
+    return 0;
+}
