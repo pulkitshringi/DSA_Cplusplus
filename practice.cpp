bd11ab802616b86ed9b3a5acf0635ad592e7fcd4
Pulkit Shringi <pulkitshringi@yahoo.com>
2024-05-30 23:12:49 +0530

    up

--- practice.cpp
+++ practice.cpp
@@ -1,21 +1,11 @@
-/*
-Pattern
-N = 4
-1
-22
-333
-4444
-*/
-#include<iostream>
+#include <iostream>
 using namespace std;
-
 int main(){
-  int i,j,n;
-    cin>>n;
-    for(i=1;i<=n;i++){
-        for(j=1;j<=i;j++){
-	        cout<<i;
-        }
-    cout<<endl;
+    int n,fact=1;
+    cin >> n;
+    for(int i=n;i>1;i--){
+        fact*=i;
     }
+    cout << fact << endl;
+    return 0;
 }
