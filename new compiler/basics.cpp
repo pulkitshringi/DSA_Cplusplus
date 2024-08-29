5acf851b3258aa6eb42323fd2a61bc7c19252b1e
Pulkit Shringi <pulkitshringi@yahoo.com>
2024-08-29 10:28:34 +0530

    updated

--- new compiler/basics.cpp
+++ new compiler/basics.cpp
@@ -1,14 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-  stack<int> s;
-  s.push(1);
-  s.push(2);
-  s.push(3);
-  cout << "Stack elements : ";
-  while(!s.empty()){
-    cout << s.top() << " ";
-    s.pop();
-  }
-  return 0;
+set<int> st;
+st.insert(43);
+st.insert(12);
+st.insert(15);
+st.insert(15);
+st.insert(2);
+auto it = st.upper_bound(2);
+if(it!=st.end()){
+  cout << *it;
+}
+else{
+  cout << "no element";
+}
+
+return 0;
 }
